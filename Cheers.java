e5c68105a16973a8817cd1ef79adca985eddae2a
שלו קרביס <shalev@shalevs-MacBook-Pro.local>
2024-11-14 20:43:49 +0200

    fixed question 3

--- Cheers.java
+++ Cheers.java
@@ -5,25 +5,27 @@ public class Cheers {
             String wordUpperCase = word.toUpperCase();
             int howManyTimes = Integer.parseInt(args[1]);
 
-            //printing the first lines
-            for (int i = 0 ; i < word.length() ; i++){
-                System.out.print("Give me ");
-            //checking if we need to use a or an
-                if ("AEFHILMNORSX".indexOf(wordUpperCase.charAt(i)) != -1){
-                        System.out.print("an ");
-                } else {
-                        System.out.print("a ");
+            for (int j = 0 ; j < 2 ; j++){
+                //printing the first lines
+                for (int i = 0 ; i < word.length() ; i++){
+                        System.out.print("Give me ");
+                //checking if we need to use a or an
+                        if ("AEFHILMNORSX".indexOf(wordUpperCase.charAt(i)) != -1){
+                                System.out.print("an ");
+                        } else {
+                                System.out.print("a ");
+                        }
+                        System.out.println(wordUpperCase.charAt(i) + ": " + wordUpperCase.charAt(i) + "!"); 
                 }
-                System.out.println(wordUpperCase.charAt(i) + ": " + wordUpperCase.charAt(i) + "!"); 
+
+                //printing the middle line
+                System.out.println("What does that spell?");
+
+                //printing the last lines
+                for (int i = 0 ; i < howManyTimes ; i++){
+                        System.out.println(wordUpperCase + "!!!");
+                }
+
             }
-
-            //printing the middle line
-            System.out.println("What does that spell?");
-
-            //printing the last lines
-            for (int i = 0 ; i < howManyTimes ; i++){
-                System.out.println(wordUpperCase + "!!!");
-            }
-
         }
 }
