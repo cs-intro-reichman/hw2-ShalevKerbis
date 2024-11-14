8d73bf430e60882c7562b05589f178d39b800e24
שלו קרביס <shalev@shalevs-MacBook-Pro.local>
2024-11-14 21:43:13 +0200

    fixed again question 3

--- Cheers.java
+++ Cheers.java
@@ -5,27 +5,23 @@ public class Cheers {
             String wordUpperCase = word.toUpperCase();
             int howManyTimes = Integer.parseInt(args[1]);
 
-            for (int j = 0 ; j < 2 ; j++){
-                //printing the first lines
-                for (int i = 0 ; i < word.length() ; i++){
-                        System.out.print("Give me ");
-                //checking if we need to use a or an
-                        if ("AEFHILMNORSX".indexOf(wordUpperCase.charAt(i)) != -1){
-                                System.out.print("an ");
-                        } else {
-                                System.out.print("a ");
-                        }
-                        System.out.println(wordUpperCase.charAt(i) + ": " + wordUpperCase.charAt(i) + "!"); 
+            
+            //printing the first lines
+            for (int i = 0 ; i < word.length() ; i++){
+            //checking if we need to use a or an
+                if ("AEFHILMNORSX".indexOf(wordUpperCase.charAt(i)) != -1){
+                        System.out.println("Give me an " + wordUpperCase.charAt(i) + ": " + wordUpperCase.charAt(i) + "!");
+                } else {
+                        System.out.println("Give me a + " + wordUpperCase.charAt(i) + ": " + wordUpperCase.charAt(i) + "!");
                 }
+            }
 
-                //printing the middle line
-                System.out.println("What does that spell?");
+            //printing the middle line
+            System.out.println("What does that spell?");
 
-                //printing the last lines
-                for (int i = 0 ; i < howManyTimes ; i++){
-                        System.out.println(wordUpperCase + "!!!");
-                }
-
+            //printing the last lines
+            for (int i = 0 ; i < howManyTimes ; i++){
+                System.out.println(wordUpperCase + "!!!");
             }
         }
 }
