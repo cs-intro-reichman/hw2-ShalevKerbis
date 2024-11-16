b33f13da723c48acf6e039f9c162059e9b7edc55
שלו קרביס <shalev@shalevs-MacBook-Pro.local>
2024-11-16 09:02:11 +0200

    completed question 4

--- CalcPi.java
+++ CalcPi.java
@@ -3,33 +3,22 @@ public class CalcPi {
 	public static void main(String [] args) { 
 	    int input = Integer.parseInt(args[0]);
 		double sum = 0;
-		double difference = 0;
-		int differenceDenominator = 3;
-		int sumDenominator = 1;
+		boolean plusMin = true;
+		int denominator = 1;
 
-		//for even input:
-		if (input % 2 == 0){
-			for (int i = 0 ; i < input / 2 ; i++){
-				difference = difference + ((double) 1 / differenceDenominator);
-				differenceDenominator = differenceDenominator + 4;
+		for (int i = 0 ; i < input ; i++){
+			if (plusMin){
+				sum = sum + (double) 1 / denominator;
+				plusMin = false;
+			} else {
+				sum = sum - (double) 1 / denominator;
+				plusMin = true;
 			}
-			for (int i = 0 ; i < input / 2 ; i++){
-				sum = sum + ((double) 1 / sumDenominator);
-				sumDenominator = sumDenominator + 4;
-			}
-		} else {
-		//for odd input:
-			for (int i = 0 ; i < input / 2 ; i++){
-				difference = difference + ((double) 1 / differenceDenominator);
-				differenceDenominator = differenceDenominator + 4;
-			}
-			for (int i = 0 ; i <= input / 2 ; i++){
-				sum = sum + ((double) 1 / sumDenominator);
-				sumDenominator = sumDenominator + 4;
-			}
+			denominator = denominator + 2;
 		}
-		double piApproximated = 4 * (double) (sum - difference);
+
+		double piApproximated = 4 * sum;
 		System.out.println("pi, according to Java: " + Math.PI);
-		System.out.println("pi, approximated:     " + piApproximated);
+		System.out.println("pi, approximated:      " + piApproximated);
 	}
 }
